e99596d5524a6433ee361e831510d9334a1d4251
Ealbarran <ealbarran2005.10@gmail.com>
2023-02-23 17:39:52 -0600

    Actualización 23/02/23

---
+++ Java/Java01/Java-Terminal/Hola.java
@@ -0,0 +1,13 @@
+class Hola{
+
+	public static void main (String [] CH23) {
+	
+		//System.out.println("Hola Java");
+
+		Saludo objSaludo;
+		objSaludo = new Saludo();
+		objSaludo.saludar();
+		
+		}
+
+	}
---
+++ Java/Java01/Java-Terminal/Saludo.java
@@ -0,0 +1,8 @@
+class Saludo {
+
+	void saludar () {
+
+		System.out.println("Hola 2 Java");
+
+	}
+}
